d2f209746a02ded6c56a91281daad5327d726aee
Benoit Myard <myardbenoit@gmail.com>
2016-10-13 03:57:28 +0200

    Bump version number

--- diamond.py
+++ diamond.py
@@ -18,7 +18,7 @@
 # this program; if not, write to the Free Software Foundation, Inc., 59 Temple
 # Place, Suite 330, Boston, MA 02111-1307 USA
 
-__version__ = '0.1';
+__version__ = '0.3';
 
 import os, re, errno, urllib
 
