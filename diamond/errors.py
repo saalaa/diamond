cd1e949617a481da9ac76b4c45287e3cc4f7c8ae
Benoit Myard <myardbenoit@gmail.com>
2016-10-24 00:03:21 +0200

    Added generic error handling

--- diamond/errors.py
+++ diamond/errors.py
@@ -1,9 +1,7 @@
-import sqlalchemy
-
 from flask import render_template
 
 from .app import app
 
-@app.errorhandler(sqlalchemy.exc.OperationalError)
+@app.errorhandler(Exception)
 def handle_bad_request(e):
-    return render_template('error.j2', error='database'), 500
+    return render_template('error.j2', error='default'), 500
