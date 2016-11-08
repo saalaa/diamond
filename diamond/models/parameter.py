d79f97ccd22e992897e029ad25713fece72cd0d0
Benoit Myard <myardbenoit@gmail.com>
2016-11-08 20:02:33 +0100

    Updated licensing

--- diamond/models/parameter.py
+++ diamond/models/parameter.py
@@ -1,3 +1,22 @@
+# Copyright (C) 1999, 2000 Martin Pool <mbp@humbug.org.au>
+# Copyright (C) 2003 Kimberley Burchett <http://www.kimbly.com>
+# Copyright (C) 2016 Benoit Myard <myardbenoit@gmail.com>
+#
+# This file is part of Diamond wiki.
+#
+# Diamond wiki is free software: you can redistribute it and/or modify it under
+# the terms of the GNU General Public License as published by the Free Software
+# Foundation, either version 3 of the License, or (at your option) any later
+# version.
+#
+# Diamond wiki is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+# details.
+#
+# You should have received a copy of the GNU General Public License along with
+# Diamond wiki. If not, see <http://www.gnu.org/licenses/>.
+
 import time
 
 from diamond.db import db
--- scripts/less.sh
+++ scripts/less.sh
@@ -1,5 +1,24 @@
 #!/bin/sh
 
+# Copyright (C) 1999, 2000 Martin Pool <mbp@humbug.org.au>
+# Copyright (C) 2003 Kimberley Burchett <http://www.kimbly.com>
+# Copyright (C) 2016 Benoit Myard <myardbenoit@gmail.com>
+#
+# This file is part of Diamond wiki.
+#
+# Diamond wiki is free software: you can redistribute it and/or modify it under
+# the terms of the GNU General Public License as published by the Free Software
+# Foundation, either version 3 of the License, or (at your option) any later
+# version.
+#
+# Diamond wiki is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+# details.
+#
+# You should have received a copy of the GNU General Public License along with
+# Diamond wiki. If not, see <http://www.gnu.org/licenses/>.
+
 ROOT=diamond/static
 
 if [ "$1" = "watch" ]; then
