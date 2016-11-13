019ff606b860955f569ef4741496e9092a86e0a1
Benoit Myard <myardbenoit@gmail.com>
2016-11-13 22:38:17 +0100

    Removed unused pytest module from tests

--- tests/test_diff.py
+++ tests/test_diff.py
@@ -17,8 +17,6 @@
 # You should have received a copy of the GNU General Public License along with
 # Diamond wiki. If not, see <http://www.gnu.org/licenses/>.
 
-import pytest
-
 from diamond import app
 from diamond.diff import unified_diff
 
