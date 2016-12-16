10cb959fc21a3841dc47d2d8ff8708c33073e738
Benoit Myard <myardbenoit@gmail.com>
2016-12-17 00:32:23 +0100

    Updated requirements (+ script to update requirements.txt)

---
+++ scripts/update-requirements.sh
@@ -0,0 +1,35 @@
+#!/bin/sh -e
+
+# Copyright (C) 1999, 2000 Martin Pool <mbp@humbug.org.au>
+# Copyright (C) 2003 Kimberley Burchett <http://www.kimbly.com>
+# Copyright (C) 2016 Benoit Myard <myardbenoit@gmail.com>
+#
+# This file is part of Diamond wiki.
+#
+# Diamond wiki is free software: you can redistribute it and/or modify it under
+# the terms of the GNU General Public License as published by the Free Software
+# Foundation, either version 3 of the License, or (at your option) any later
+# version.
+#
+# Diamond wiki is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+# details.
+#
+# You should have received a copy of the GNU General Public License along with
+# Diamond wiki. If not, see <http://www.gnu.org/licenses/>.
+
+NEW_REQUIREMENTS=`pip list --outdated --format legacy | \
+  awk '{ print $1 "==" $5; }'`
+
+for line in $NEW_REQUIREMENTS; do
+  library=`echo $line | awk -F= '{ print $1; }'`
+
+  echo $library
+
+  # Some `sed` implementations don't support `-i` properly.
+  sed "s/^$library==.*/$line/" requirements.txt > \
+    /tmp/requirements.txt.$$
+
+  mv /tmp/requirements.txt.$$ requirements.txt
+done
